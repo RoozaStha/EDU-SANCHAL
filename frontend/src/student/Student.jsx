fde77fcd434bfc789578aa258bc9af6c59e5442b
RoozaStha <stharooza3@gmail.com>
2025-05-15 20:35:24 +0545

    logout implemented on all dashboard teacher, student, school

--- frontend/src/student/Student.jsx
+++ frontend/src/student/Student.jsx
@@ -1,76 +1,73 @@
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import { Outlet, useNavigate } from 'react-router-dom';
-import DashboardCustomizeIcon from '@mui/icons-material/DashboardCustomize'; //dashboardcustomize icon
-import PeopleIcon from '@mui/icons-material/People';//student
-import PeopleAltIcon from '@mui/icons-material/PeopleAlt';//teacher
-import EventIcon from '@mui/icons-material/Event';//schedule
-import NotificationsIcon from '@mui/icons-material/Notifications';//notification
-import SubjectIcon from '@mui/icons-material/Subject';//subject
-import ExplicitIcon from '@mui/icons-material/Explicit';//examinations
-import RecentActorsIcon from '@mui/icons-material/RecentActors';//attendance
-import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';//class
-import HomeIcon from '@mui/icons-material/Home';
-
-
-
-
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import { Outlet, useNavigate } from "react-router-dom";
+import DashboardCustomizeIcon from "@mui/icons-material/DashboardCustomize"; //dashboardcustomize icon
+import PeopleIcon from "@mui/icons-material/People"; //student
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt"; //teacher
+import EventIcon from "@mui/icons-material/Event"; //schedule
+import NotificationsIcon from "@mui/icons-material/Notifications"; //notification
+import SubjectIcon from "@mui/icons-material/Subject"; //subject
+import ExplicitIcon from "@mui/icons-material/Explicit"; //examinations
+import RecentActorsIcon from "@mui/icons-material/RecentActors"; //attendance
+import LocalLibraryIcon from "@mui/icons-material/LocalLibrary"; //class
+import HomeIcon from "@mui/icons-material/Home";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const drawerWidth = 240;
 
 const openedMixin = (theme) => ({
   width: drawerWidth,
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.enteringScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
 });
 
 const closedMixin = (theme) => ({
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
   width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
+  [theme.breakpoints.up("sm")]: {
     width: `calc(${theme.spacing(8)} + 1px)`,
   },
 });
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
 }));
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme }) => ({
   zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
+  transition: theme.transitions.create(["width", "margin"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
@@ -80,7 +77,7 @@ const AppBar = styled(MuiAppBar, {
       style: {
         marginLeft: drawerWidth,
         width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
+        transition: theme.transitions.create(["width", "margin"], {
           easing: theme.transitions.easing.sharp,
           duration: theme.transitions.duration.enteringScreen,
         }),
@@ -89,30 +86,30 @@ const AppBar = styled(MuiAppBar, {
   ],
 }));
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    variants: [
-      {
-        props: ({ open }) => open,
-        style: {
-          ...openedMixin(theme),
-          '& .MuiDrawer-paper': openedMixin(theme),
-        },
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  variants: [
+    {
+      props: ({ open }) => open,
+      style: {
+        ...openedMixin(theme),
+        "& .MuiDrawer-paper": openedMixin(theme),
       },
-      {
-        props: ({ open }) => !open,
-        style: {
-          ...closedMixin(theme),
-          '& .MuiDrawer-paper': closedMixin(theme),
-        },
+    },
+    {
+      props: ({ open }) => !open,
+      style: {
+        ...closedMixin(theme),
+        "& .MuiDrawer-paper": closedMixin(theme),
       },
-    ],
-  }),
-);
+    },
+  ],
+}));
 
 export default function Student() {
   const theme = useTheme();
@@ -126,24 +123,36 @@ export default function Student() {
     setOpen(false);
   };
 
-  
   const navArr = [
     { link: "/", component: "Home", icon: HomeIcon },
 
-    { link: "/student", component: "Your Details", icon: DashboardCustomizeIcon },
+    {
+      link: "/student",
+      component: "Your Details",
+      icon: DashboardCustomizeIcon,
+    },
     { link: "/student/schedule", component: "Schedule", icon: EventIcon },
-    { link: "/student/attendance", component: "Attendance", icon: RecentActorsIcon },
-    { link: "/student/examinations", component: "Examinations", icon: ExplicitIcon },
-    { link: "/student/notice", component: "Notice", icon: NotificationsIcon }
-  ]; 
-  
-const navigate = useNavigate()
-  const handleNavigation =(link)=>{
-    navigate(link)
-  }
+    {
+      link: "/student/attendance",
+      component: "Attendance",
+      icon: RecentActorsIcon,
+    },
+    {
+      link: "/student/examinations",
+      component: "Examinations",
+      icon: ExplicitIcon,
+    },
+    { link: "/student/notice", component: "Notice", icon: NotificationsIcon },
+    { link: "/logout", component: "Logout", icon: LogoutIcon },
+  ];
+
+  const navigate = useNavigate();
+  const handleNavigation = (link) => {
+    navigate(link);
+  };
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar position="fixed" open={open}>
         <Toolbar>
@@ -156,27 +165,30 @@ const navigate = useNavigate()
               {
                 marginRight: 5,
               },
-              open && { display: 'none' },
+              open && { display: "none" },
             ]}
           >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap component="div">
             School Management System [MULTIPLE]
-
           </Typography>
         </Toolbar>
       </AppBar>
       <Drawer variant="permanent" open={open}>
         <DrawerHeader>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
           </IconButton>
         </DrawerHeader>
         <Divider />
         <List>
           {navArr.map((navItem, index) => (
-            <ListItem key={index} disablePadding sx={{ display: 'block' }}>
+            <ListItem key={index} disablePadding sx={{ display: "block" }}>
               <ListItemButton
                 sx={[
                   {
@@ -185,30 +197,32 @@ const navigate = useNavigate()
                   },
                   open
                     ? {
-                        justifyContent: 'initial',
+                        justifyContent: "initial",
                       }
                     : {
-                        justifyContent: 'center',
+                        justifyContent: "center",
                       },
                 ]}
-                onClick={()=>{handleNavigation(navItem.link)}}
+                onClick={() => {
+                  handleNavigation(navItem.link);
+                }}
               >
                 <ListItemIcon
                   sx={[
                     {
                       minWidth: 0,
-                      justifyContent: 'center',
+                      justifyContent: "center",
                     },
                     open
                       ? {
                           mr: 3,
                         }
                       : {
-                          mr: 'auto',
+                          mr: "auto",
                         },
                   ]}
                 >
-                  {<navItem.icon/>}
+                  {<navItem.icon />}
                 </ListItemIcon>
                 <ListItemText
                   primary={navItem.component}
@@ -281,8 +295,7 @@ const navigate = useNavigate()
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <DrawerHeader />
-        <Outlet/>
-        
+        <Outlet />
       </Box>
     </Box>
   );
